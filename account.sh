7382329d95b7263bc6c49491153ad0ef01dee5b4
xxaxxelxx <x@axxel.net>
2015-12-10 16:32:36 +0100

    updated

--- account.sh
+++ account.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SPLITBASEDIR="/customer"
 CUSTOMER=$1
-PRICE_PER_MBYTE_IN_CENT=$2
+PRICE_PER_MBYTE_IN_CENT=$( echo "$2" | sed 's|\,|\.|g')
 SLEEP=60
 
 test -d $SPLITBASEDIR/$CUSTOMER/logs || exit
@@ -9,7 +9,7 @@ test -d $SPLITBASEDIR/$CUSTOMER/logs || exit
 while true; do
     test -r $SPLITBASEDIR/$CUSTOMER/logs/$(date +%Y_%m).bytesum || ( sleep $SLEEP; continue )
     MBYTES=$(echo "$(cat $SPLITBASEDIR/$CUSTOMER/logs/$(date +%Y_%m).bytesum) / ( 1024 * 1024 )" | bc) #"
-    ACCOUNT=$(echo "( $MBYTES * $PRICE_PER_MBYTE ) / 100" | bc) #"
+    ACCOUNT=$(echo "scale=2;( $MBYTES * $PRICE_PER_MBYTE_IN_CENT ) / 100" | bc) #"
     echo $ACCOUNT > $SPLITBASEDIR/$CUSTOMER/logs/$(date +%Y_%m).account
 sleep $SLEEP
 done
