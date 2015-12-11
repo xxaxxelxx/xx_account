6c3c15ddc1b6bdb74f615dc3e24342083fcf2d30
xxaxxelxx <x@axxel.net>
2015-12-11 13:06:49 +0100

    updated

--- account.sh
+++ account.sh
@@ -7,15 +7,23 @@ SLEEP=60
 test -d $SPLITBASEDIR/$CUSTOMER/logs || exit
 
 while true; do
-    test -r $SPLITBASEDIR/$CUSTOMER/logs/$(date +%Y_%m).bytesum
-    if [ $? -eq 0 ]; then
-	cat $SPLITBASEDIR/$CUSTOMER/logs/$(date +%Y_%m).bytesum | grep '[[:digit:]]' > /dev/null
+    TOFFSET=0
+    while true; do
+	if [ $TOFFSET -gt 86400 ]; then break; fi
+	NOW=$(date +%s)
+	TSTAMP=$(date -d @$(($NOW - $TOFFSET)) +%Y_%m)
+	if $TSTA
+	test -r $SPLITBASEDIR/$CUSTOMER/logs/$TSTAMP.bytesum
 	if [ $? -eq 0 ]; then
-	    MBYTES=$(echo "$(cat $SPLITBASEDIR/$CUSTOMER/logs/$(date +%Y_%m).bytesum) / ( 1024 * 1024 )" | bc) #"
-	    ACCOUNT=$(echo "scale=2;( $MBYTES * $PRICE_PER_MBYTE ) / 100" | bc) #"
-	    echo "$ACCOUNT Euro for $MBYTES MBytes" | sed 's|^\.|0\.|' > $SPLITBASEDIR/$CUSTOMER/logs/$(date +%Y_%m).account.txt
+	    cat $SPLITBASEDIR/$CUSTOMER/logs/$TSTAMP.bytesum | grep '[[:digit:]]' > /dev/null
+	    if [ $? -eq 0 ]; then
+		MBYTES=$(echo "$(cat $SPLITBASEDIR/$CUSTOMER/logs/$TSTAMP.bytesum) / ( 1024 * 1024 )" | bc) #"
+		ACCOUNT=$(echo "scale=2;( $MBYTES * $PRICE_PER_MBYTE ) / 100" | bc) #"
+		echo "$ACCOUNT Euro for $MBYTES MBytes" | sed 's|^\.|0\.|' > $SPLITBASEDIR/$CUSTOMER/logs/$TSTAMP.account.txt
+	    fi
 	fi
-    fi
+	TOFFSET=$(($TOFFSET + 86400))
+    done
     sleep $SLEEP
 done
 exit
