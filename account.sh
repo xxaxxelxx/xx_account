52578b847f8c1a6266d7dea0f7a5cd95131077d0
xxaxxelxx <x@axxel.net>
2015-12-15 14:17:49 +0100

    updated

--- account.sh
+++ account.sh
@@ -1,27 +1,79 @@
 #!/bin/bash
 SPLITBASEDIR="/customer"
 CUSTOMER=$1
-PRICE_PER_MBYTE="$(echo $2 | sed 's|\,|\.|g')"
+PRICELIST_PER_GBYTE_IN_EURO="$(echo $2 | sed 's|\,|\.|g')"
+DISCOUNTTYPE="$3"
+OIFS=$IFS;IFS=$'|';A_PRICELIST_PER_GBYTE_IN_EURO=($PRICELIST_PER_GBYTE_IN_EURO);IFS=$OIFS
+for STEP in "${A_PRICELIST_PER_GBYTE_IN_EURO[@]}"; do
+    A_PRICELIST_PER_GBYTE_IN_EURO_REV=($STEP ${A_PRICELIST_PER_GBYTE_IN_EURO_REV[@]})
+done
+
 SLEEP=60
 
-test -d $SPLITBASEDIR/$CUSTOMER/logs || exit
+#WORKDIR="$SPLITBASEDIR/$CUSTOMER/logs"
+WORKDIR="/mnt/docker/HOMEGROWN/TEST"
+test -d $WORKDIR || exit
 
 while true; do
-    TOFFSET=0
-    while [ $TOFFSET -lt 86401 ]; do
-	NOW=$(date +%s)
-	TSTAMP=$(date -d @$(($NOW - $TOFFSET)) +%Y_%m)
-	test -r $SPLITBASEDIR/$CUSTOMER/logs/$TSTAMP.bytesum
-	if [ $? -eq 0 ]; then
-	    cat $SPLITBASEDIR/$CUSTOMER/logs/$TSTAMP.bytesum | grep '[[:digit:]]' > /dev/null
+    case $DISCOUNTTYPE in
+    retroactive)
+	TOFFSET=0
+	while [ $TOFFSET -lt 86401 ]; do
+	    NOW=$(date +%s)
+	    TSTAMP=$(date -d @$(($NOW - $TOFFSET)) +%Y_%m)
+	    test -r $WORKDIR/$TSTAMP.bytesum
 	    if [ $? -eq 0 ]; then
-		MBYTES=$(echo "$(cat $SPLITBASEDIR/$CUSTOMER/logs/$TSTAMP.bytesum) / ( 1024 * 1024 )" | bc) #"
-		ACCOUNT=$(echo "scale=2;( $MBYTES * $PRICE_PER_MBYTE ) / 100" | bc) #"
-		echo "$ACCOUNT Euro for $MBYTES MBytes" | sed 's|^\.|0\.|' > $SPLITBASEDIR/$CUSTOMER/logs/$TSTAMP.account.txt
+		cat $WORKDIR/$TSTAMP.bytesum | grep '[[:digit:]]' > /dev/null
+		if [ $? -eq 0 ]; then
+		    GBYTES=$(echo "$(cat $WORKDIR/$TSTAMP.bytesum) / ( 1024 * 1024 * 1024 )" | bc) #"
+		    PRICE=0
+		    for STEP in "${A_PRICELIST_PER_GBYTE_IN_EURO[@]}"; do
+			OIFS=$IFS;IFS=$'#';A_PRICE_PER_GBYTE_IN_EURO=($STEP);IFS=$OIFS
+			BEYOND=${A_PRICE_PER_GBYTE_IN_EURO[0]}
+			PRICE=${A_PRICE_PER_GBYTE_IN_EURO[1]}
+			if [ $GBYTES -ge $BEYOND ]; then
+			    ACCOUNT=$(echo "scale=2;( $GBYTES * $PRICE )" | bc) #"
+			fi    
+		    done
+		    GBYTES_PRINT=$(echo $GBYTES | sed 's/\(...\)$/\.\1/') #'
+		    echo "Actually you have to pay $ACCOUNT Euro for $GBYTES_PRINT Gbytes. ($PRICE Euro per GByte)" > $WORKDIR/$TSTAMP.account.txt
+		fi
 	    fi
-	fi
-	TOFFSET=$(($TOFFSET + 86400))
-    done
+	    TOFFSET=$(($TOFFSET + ( 86400 * 31 )))
+	done
+    ;;
+    *)
+	TOFFSET=0
+	while [ $TOFFSET -lt 86401 ]; do
+	    NOW=$(date +%s)
+	    TSTAMP=$(date -d @$(($NOW - $TOFFSET)) +%Y_%m)
+	    test -r $WORKDIR/$TSTAMP.bytesum
+	    if [ $? -eq 0 ]; then
+		cat $WORKDIR/$TSTAMP.bytesum | grep '[[:digit:]]' > /dev/null
+		if [ $? -eq 0 ]; then
+		    GBYTES=$(echo "$(cat $WORKDIR/$TSTAMP.bytesum) / ( 1024 * 1024 * 1024 )" | bc) #"
+		    ACCOUNT=0
+		    GBYTES_TOTAL=$GBYTES
+		    for STEP in "${A_PRICELIST_PER_GBYTE_IN_EURO_REV[@]}"; do
+			OIFS=$IFS;IFS=$'#';A_PRICE_PER_GBYTE_IN_EURO=($STEP);IFS=$OIFS
+			BEYOND=${A_PRICE_PER_GBYTE_IN_EURO[0]}
+			PRICE=${A_PRICE_PER_GBYTE_IN_EURO[1]}
+			if [ $GBYTES -ge $BEYOND ]; then
+			    GBYTES_COUNTED=$(($GBYTES - $BEYOND))
+			    ACCOUNT=$(echo "scale=2;( ( $GBYTES_COUNTED * $PRICE ) + $ACCOUNT )" | bc) #"
+			    GBYTES=$BEYOND
+			fi    
+		    done
+		    GBYTES_PRINT=$(echo $GBYTES_TOTAL | sed 's/\(...\)$/\.\1/') #'
+		    echo "Actually you have to pay $ACCOUNT Euro for $GBYTES_PRINT Gbytes." > $WORKDIR/$TSTAMP.account.txt
+		fi
+	    fi
+	    TOFFSET=$(($TOFFSET + ( 86400 * 31 )))
+	done
+    ;;
+    esac
+
+    exit
     sleep $SLEEP
 done
 exit
